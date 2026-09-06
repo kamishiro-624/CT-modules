9497bf4214212f8b4cd3e894f0180dbc229bde92
kamishiro <xkamishiro624@gmail.com>
2026-09-06 15:05:32 -0400

    Simplify chat command registration syntax

--- autoCollect/index.js
+++ autoCollect/index.js
@@ -1,1 +1,1 @@
-register("chat", () => {ChatLib.say("/collect");}).setCriteria("* First person to type /collect will earn free coins!").setContains();
+register("chat", () => ChatLib.say("/collect")).setCriteria("* First person to type /collect will earn free coins!").setContains();
